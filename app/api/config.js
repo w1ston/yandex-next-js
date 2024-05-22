c7ebf378585de8a7d0ba703ae4434667e529d726
minip <petrov-valera05@mail.ru>
2024-05-22 19:07:51 +0300

    ...

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pindieback.nomoredomainswork.ru/api';
+export const BASE_URL = 'https://pindiebackwistonss.nomoredomainswork.ru/api';
 
 export const endpoints = {
     games: `${BASE_URL}/games`,
