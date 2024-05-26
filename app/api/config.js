736c5b158f0b73e86a49fa2924f9342489a656f4
minip <petrov-valera05@mail.ru>
2024-05-26 19:10:06 +0300

    Финальное ревью

--- app/api/config.js
+++ app/api/config.js
@@ -3,5 +3,5 @@ export const BASE_URL = 'https://pindiebackwistonss.nomoredomainswork.ru/api';
 export const endpoints = {
     games: `${BASE_URL}/games`,
     auth: `${BASE_URL}/auth/login`,
-    me: `${BASE_URL}/users/me`
+    me: `${BASE_URL}/me`
 };
